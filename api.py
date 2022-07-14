e60d5165c549ae4a18860ca87f66557b5ac88f9b
hlongtr01 <hlongtr01@gmail.com>
2022-07-14 10:01:34 +0700

    api

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from pyspark.conf import SparkConf
 spark_conf = SparkConf() \
         .setAppName("api") \
         .setMaster("spark://192.168.245.130:7077") \
-        .set("spark.executor.memory", "2GB") \
+        .set("spark.executor.memory", "8GB") \
         .set("spark.executor.cores", 1) \
         .set("spark.shuffle.service.enabled", "false") \
         .set("spark.dynamicAllocation.enabled", "false")   
